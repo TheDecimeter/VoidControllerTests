fdcbda4b6c069a86150e8245b2215f37f0181a4d
theDecimeter <daniel@curiousorigins.com>
2024-05-03 16:51:31 -0700

    Added simple tests from tutorial, starting to source code for track pad

--- GlobalActionBarService/app/src/main/java/com/example/android/globalactionbarservice/GlobalActionBarService.java
+++ GlobalActionBarService/app/src/main/java/com/example/android/globalactionbarservice/GlobalActionBarService.java
@@ -18,10 +18,13 @@ import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.GestureDescription;
 import android.graphics.Path;
 import android.graphics.PixelFormat;
+import android.graphics.PointF;
 import android.media.AudioManager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
@@ -32,14 +35,140 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class GlobalActionBarService extends AccessibilityService {
+    FrameLayout mLayout;
+
+
+    @Override
+    protected void onServiceConnected() {
+        // Create an overlay and display the action bar
+        Log.d("DANX", "onServiceConnected");
+        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mLayout = new FrameLayout(this);
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.type = WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY;
+        lp.format = PixelFormat.TRANSLUCENT;
+        lp.flags |= WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.gravity = Gravity.TOP;
+        LayoutInflater inflater = LayoutInflater.from(this);
+        inflater.inflate(R.layout.action_bar, mLayout);
+        wm.addView(mLayout, lp);
+
+
+        configurePowerButton();
+        configureVolumeButton();
+        configureScrollButton();
+        configureSwipeButton();
+    }
+
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.d("DANX", "onAccessibilityEvent");
 
     }
 
     @Override
     public void onInterrupt() {
+        Log.d("DANX", "onInterrupt");
 
+    }
+
+
+    /**
+     * Don't get excited, this is just a software power button
+     *
+     * but...
+     * might be useful for emmiting certain key presses globally (dpad?)
+     * might be useful for keeping soft keyboard hidden (if using keyboard on external display)
+     */
+    private void configurePowerButton() {
+        Button powerButton = (Button) mLayout.findViewById(R.id.power);
+        powerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performGlobalAction(GLOBAL_ACTION_POWER_DIALOG);
+            }
+        });
+    }
+
+    private void configureVolumeButton() {
+        Button volumeUpButton = (Button) mLayout.findViewById(R.id.volume_up);
+        volumeUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                        AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+            }
+        });
+    }
+
+
+
+    private AccessibilityNodeInfo findScrollableNode(AccessibilityNodeInfo root) {
+        Deque<AccessibilityNodeInfo> deque = new ArrayDeque<>();
+        deque.add(root);
+        while (!deque.isEmpty()) {
+            AccessibilityNodeInfo node = deque.removeFirst();
+            if (node.getActionList().contains(AccessibilityNodeInfo.AccessibilityAction.ACTION_SCROLL_FORWARD)) {
+                return node;
+            }
+            for (int i = 0; i < node.getChildCount(); i++) {
+                deque.addLast(node.getChild(i));
+            }
+        }
+        return null;
+    }
+
+    private void configureScrollButton() {
+        Button scrollButton = (Button) mLayout.findViewById(R.id.scroll);
+        scrollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AccessibilityNodeInfo scrollable = findScrollableNode(getRootInActiveWindow());
+                if (scrollable != null) {
+                    scrollable.performAction(AccessibilityNodeInfo.AccessibilityAction.ACTION_SCROLL_FORWARD.getId());
+                }
+            }
+        });
+    }
+
+    /**
+     * I think this is where the magic's at for any track pad type implementation
+     */
+    private void configureSwipeButton() {
+        Button swipeButton = (Button) mLayout.findViewById(R.id.swipe);
+        swipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Path swipePath = new Path();
+                swipePath.moveTo(1000, 1000);
+                swipePath.lineTo(100, 1000);
+                GestureDescription.Builder gestureBuilder = new GestureDescription.Builder();
+                gestureBuilder.addStroke(new GestureDescription.StrokeDescription(swipePath, 0, 500));
+                dispatchGesture(gestureBuilder.build(), null, null);
+            }
+        });
+    }
+
+    /**
+     * https://stackoverflow.com/questions/50775698/how-can-i-reliably-simulate-touch-events-on-android-without-root-like-automate
+     * @param x
+     * @param y
+     * @return
+     */
+    private GestureDescription createClick(float x, float y) {
+        // for a single tap a duration of 1 ms is enough
+        final int DURATION = 1;
+
+        Path clickPath = new Path();
+        clickPath.moveTo(x, y);
+        GestureDescription.StrokeDescription clickStroke =
+                new GestureDescription.StrokeDescription(clickPath, 0, DURATION);
+        GestureDescription.Builder clickBuilder = new GestureDescription.Builder();
+        clickBuilder.addStroke(clickStroke);
+        return clickBuilder.build();
     }
 }
